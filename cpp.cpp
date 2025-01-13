04aeab3348884a06b7f586a9b23c7b920481139b
Eliud <124616502+eliudnjoroo@users.noreply.github.com>
2025-01-13 08:14:05 +0300

    Add files via upload

---
+++ cpp.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+using namespace std;
+int main(){
+    char gender;
+    int age, salary;
+    cout<<"enter your gender:"<<endl;
+    cin>>gender;
+    cout<<"enter your age and salary:"<<endl;
+    cin>>age>>salary;
+    cout<<"user details - gender is "<<gender<<","<<endl<< "age and salry is "<<age<<" and "<<salary;
+    return 0;
+}
